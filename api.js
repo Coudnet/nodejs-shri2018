dbbb42002c2f5ce7fdf6dce919b2f19902385463
Гриша <greha1307@gmail.com>
2018-10-15 18:56:19 +0300

    Фикс пагинации

--- api.js
+++ api.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const fs = require("fs");
 
-const pageSize = 2;
+const pageSize = 2; // Размер страницы выдачи
 
+/**
+ * Класс для генерации ошибки некорректного входного параметра
+ */
 class ParamTypeError {
     constructor(errno, message) {
         this.errno = errno;
@@ -11,15 +14,17 @@ class ParamTypeError {
     }
 }
 
-const correctTypes = ['info', 'critical'];
+const correctTypes = ['info', 'critical']; //  Массив корректных параметров
 
+/**
+ * Обработчик GET запроса
+ */
 router.get('/events', function(req, res) {
-
     try {
-        checkParams(req.query.type, req.query.page);
-        getData(req.query.type, req.query.page, res);
+        checkParams(req.query.type, req.query.page); // Проверка параметров
+        getData(req.query.type, req.query.page, res); // Парметры верные, выполняеится обработка запроса
     } catch(error) {
-        if(!error.errno) error.errno = 500;
+        if(!error.errno) error.errno = 500; // Если нет кода ошибки, значит она системная и это внутренняя ошибка сервера
         res.status(error.errno).json({
             data: null,
             pagination: null,
@@ -46,6 +51,14 @@ router.post('/events', function(req, res) {
     }
 });
 
+/**
+ * Функция обработчик запроса
+ * Выполняет асинхронное чтение json, вызывает функцию для разбиения данных на страницы и
+ * формирует ответ с пагинацией
+ * @param type
+ * @param page
+ * @param res
+ */
 function getData(type, page, res) {
     fs.readFile("events.json", "utf8",
         function(error, data) {
@@ -62,18 +75,29 @@ function getData(type, page, res) {
                 data: dataPages[page - 1],
                 error: null,
                 pagination: {
-                    prevPage: (page - 1) < 1 ? null : page - 1,
+                    prevPage: ((page - 1) < 1) ? null : page - 1,
+                    currentPage: page,
                     total: dataPagesNum,
-                    nextPage: (page + 1) > dataPagesNum ? null : page + 1
+                    nextPage: ((page + 1) > dataPagesNum) ? null : page + 1
                 }
             });
         });
 }
 
+/**
+ * Функция проверка параметров. Генерит кастомный класс ошибки
+ * @param type
+ */
 function checkParams(type) {
     if(correctTypes.indexOf(type) === -1) throw new ParamTypeError(400, 'Неверный тип параметра type');
 }
 
+/**
+ * Функция для сортировки данных по нужному типу, с разбиением на страницы
+ * @param events
+ * @param type
+ * @returns {Array}
+ */
 function makePagination(events, type) {
     let dataPages = [];
     let j = 0;

681d885f2d7246626c01c90d8b6930622f051ac7
Гриша <greha1307@gmail.com>
2018-10-16 00:07:38 +0300

    Исправлена пагинация. Добавлены проверки на type и page. Добавлена обработка ошибки в коллбэке sendData()

--- api.js
+++ api.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 const pageSize = 2; // Размер страницы выдачи
 
+const correctTypes = ['info', 'critical']; //  Массив корректных параметров
+
 /**
  * Класс для генерации ошибки некорректного входного параметра
  */
@@ -14,42 +16,21 @@ class ParamTypeError {
     }
 }
 
-const correctTypes = ['info', 'critical']; //  Массив корректных параметров
-
-/**
- * Обработчик GET запроса
- */
 router.get('/events', function(req, res) {
-    try {
-        checkParams(req.query.type, req.query.page); // Проверка параметров
-        getData(req.query.type, req.query.page, res); // Парметры верные, выполняеится обработка запроса
-    } catch(error) {
-        if(!error.errno) error.errno = 500; // Если нет кода ошибки, значит она системная и это внутренняя ошибка сервера
-        res.status(error.errno).json({
-            data: null,
-            pagination: null,
-            error: {
-                msg: error.message
-            }
-        })
-    }
+    sendResponse(res, req.query.type, req.query.page);
 });
 
 router.post('/events', function(req, res) {
-    try {
-        checkParams(req.body.type, req.body.page);
-        getData(req.body.type, req.body.page, res);
-    } catch(error) {
-        if(!error.errno) error.errno = 500;
-        res.status(error.errno).json({
-            data: null,
-            pagination: null,
-            error: {
-                msg: error.message
-            }
-        })
-    }
+    sendResponse(res, req.body.type, req.body.page);
 });
+
+function sendResponse(res, type, page) {
+    try {
+        sendData(res, type, page);
+    } catch(error) {
+        sendError(res, error);
+    }
+}
 
 /**
  * Функция обработчик запроса
@@ -59,41 +40,71 @@ router.post('/events', function(req, res) {
  * @param page
  * @param res
  */
-function getData(type, page, res) {
+function sendData(res, type, page) {
+    let params = checkParams(type, page);
+
     fs.readFile("events.json", "utf8",
         function(error, data) {
-            if(error) throw error; // если возникла ошибка
+            try {
+                if(error) throw error; // Не уверен, что это хорошая практика, но чтобы не плодить if и обработать исключение
 
-            const dataJSON = JSON.parse(data);
-            let dataPages = makePagination(dataJSON.events, type);
+                const dataJSON = JSON.parse(data);
+                let dataPages = makePagination(dataJSON.events, params.type);
 
-            const dataPagesNum = dataPages.length;
-            if(page < 1) page = 1;
-            if(page > dataPagesNum) page = dataPagesNum;
+                const dataPagesNum = dataPages.length;
 
-            res.json({
-                data: dataPages[page - 1],
-                error: null,
-                pagination: {
-                    prevPage: ((page - 1) < 1) ? null : page - 1,
-                    currentPage: page,
-                    total: dataPagesNum,
-                    nextPage: ((page + 1) > dataPagesNum) ? null : page + 1
-                }
-            });
+                if(params.page < 1) params.page = 1;
+                if(params.page > dataPagesNum) params.page = dataPagesNum;
+
+                res.json({
+                    data: dataPages[params.page - 1],
+                    error: null,
+                    pagination: {
+                        prevPage: ((params.page - 1) < 1) ? null : (params.page - 1),
+                        currentPage: params.page,
+                        total: dataPagesNum,
+                        nextPage: ((params.page + 1) > dataPagesNum) ? null : (params.page + 1)
+                    }
+                });
+            } catch(error) {
+                sendError(res, error);
+            }
         });
 }
 
+function sendError(res, error) {
+    if(!error.errno) error.errno = 500;
+    res.status(error.errno).json({
+        data: null,
+        pagination: null,
+        error: {
+            msg: error.message
+        }
+    })
+}
+
+
 /**
- * Функция проверка параметров. Генерит кастомный класс ошибки
+ * Функция проверки и нормирования параметров
+ * Если type отсутствует, то выбираются любые события
+ * Проверка на корректный page. Пагинация есть всегда
+ *
  * @param type
+ * @param page
+ * @returns {{type: String, page: number}}
  */
-function checkParams(type) {
-    if(correctTypes.indexOf(type) === -1) throw new ParamTypeError(400, 'Неверный тип параметра type');
+function checkParams(type, page) {
+    if((correctTypes.indexOf(type) === -1) && type) throw new ParamTypeError(400, 'Неверный тип параметра type');
+    if(!type) type = 'all';
+    if(!page || !(+page instanceof Number)) page = 1;
+    return {
+        type: type,
+        page: +page
+    }
 }
 
 /**
- * Функция для сортировки данных по нужному типу, с разбиением на страницы
+ * Функция для разбиения на страницы с сортировкой по нужному типу
  * @param events
  * @param type
  * @returns {Array}
@@ -102,8 +113,9 @@ function makePagination(events, type) {
     let dataPages = [];
     let j = 0;
     dataPages[j] = [];
+
     for (let i = 0; i < events.length; i++) {
-        if(events[i].type === type) {
+        if(events[i].type === type || type === 'all') {
             dataPages[j].push(events[i]);
             if(!(dataPages[j].length % pageSize) && dataPages[j].length) {
                 j++;
--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use('/status', statusRoute);
 
 app.use(function(req, res) {
     res.status(404).send('<h1>Page not found</h1>');
-    console.log(1);
 });
 
 module.exports = app;

875a0a2fbfe8c57422b3dde7342f661d77c93d2c
Гриша <greha1307@gmail.com>
2018-11-13 20:22:17 +0300

    Исправлен middleware для контроля CORS

--- dist/middleware/middleware.js
+++ dist/middleware/middleware.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setHeadersMiddleware = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.header("Access-Control-Allow-Origin", "http://localhost");
     next();
 };
 //# sourceMappingURL=middleware.js.map
